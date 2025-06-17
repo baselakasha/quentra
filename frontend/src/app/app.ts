627343528e2f371daa2e448eb1186c5190f376ff
Basel Akasha <contact@baselakasha.com>
2025-06-17 21:11:23 +0100

    Fix frontend compilation warnings

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Home} from './home/home';
 import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
