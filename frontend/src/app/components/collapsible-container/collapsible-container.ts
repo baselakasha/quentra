9097f4d7849b9d674681b78bb71270a83a46276e
Basel Akasha <contact@baselakasha.com>
2025-07-14 13:45:29 +0100

    Improve budget header on mobile

--- frontend/src/app/components/collapsible-container/collapsible-container.ts
+++ frontend/src/app/components/collapsible-container/collapsible-container.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, ContentChild, TemplateRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ContentChild, TemplateRef, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,12 +15,25 @@ export class CollapsibleContainerComponent {
   @Input() headerClass: string = '';
   @Input() bodyClass: string = '';
   @Input() showToggleIcon: boolean = true;
+  @Input() compactMobile: boolean = true; // Enable compact mode for mobile by default
   
   @Output() toggleExpand = new EventEmitter<boolean>();
   
   @ContentChild('headerContent') headerContent!: TemplateRef<any>;
   @ContentChild('headerActions') headerActions!: TemplateRef<any>;
   @ContentChild('bodyContent') bodyContent!: TemplateRef<any>;
+  
+  isMobile: boolean = false;
+  
+  constructor() {
+    // Set initial mobile state
+    this.checkScreenWidth();
+  }
+  
+  @HostListener('window:resize')
+  checkScreenWidth() {
+    this.isMobile = window.innerWidth < 768;
+  }
   
   toggle() {
     this.isExpanded = !this.isExpanded;
